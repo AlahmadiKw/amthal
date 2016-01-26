3b7c3a696d3692512b0f03191785da9d5af352d2
mhd1991 <m91.alahmadi@gmail.com>
2016-01-26 02:11:28 -0800

    added shell import, fixed login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def login():
         user_doc = mongo.db.users.find_one({'_id': form.username.data})
         if user_doc and \
            User.validate_login(user_doc['password_hash'], form.password.data):
-            user_obj = User(user['_id'])
+            user_obj = User(user_doc['_id'])
             login_user(user_obj)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.', category='alert-danger')
@@ -24,5 +24,5 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out.')
+    flash('You have been logged out.', category='alert-info')
     return redirect(url_for('main.index'))
--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import os
-from app import create_app
 from flask.ext.script import Manager, Shell
+
+from app import create_app
+from app import mongo
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, User=User, mongo=mongo, )
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
