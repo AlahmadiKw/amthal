451466ffac1faf9860c312d9fc9e930a12b1bba6
mhd1991 <m91.alahmadi@gmail.com>
2016-01-31 03:48:56 -0800

    finished email confirmation - email doesn't work for now

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask.ext.login import UserMixin
+from flask import current_app
 from . import mongo, login_manager
 
 
@@ -10,6 +12,28 @@ class User(UserMixin):
 
     def get_id(self):
         return self.username
+
+    def confirmed(self):
+        doc = mongo.db.users.find_one({'_id': self.username},
+                                      {'_id':0, 'confirmed': 1})
+        try:
+            return doc['confirmed']
+        except KeyError, e:
+            return False
+
+    def generate_confirmation_token(self, expiration=3600):
+        print self.username
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.username})
+
+    @property
+    def email(self):
+        doc = mongo.db.users.find_one({'_id': self.username},
+                                      {'_id':0, 'email': 1})
+        try:
+            return doc['email']
+        except KeyError, e:
+            return False
 
     @staticmethod
     def get_user(username):
@@ -29,6 +53,19 @@ class User(UserMixin):
     @staticmethod
     def validate_login(password_hash, password):
         return check_password_hash(password_hash, password)
+
+    @staticmethod
+    def confirm(username, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != username:
+            return False
+        mongo.db.users.update_one({'_id': username},
+                                  {'$set': { 'confirmed': True}})
+        return True
 
 
 @login_manager.user_loader
